74caeb0de54abe1c5cac80e3b0ac932ed44e0e8d
nickely <kknicklas3689@hotmail.com>
2022-02-10 05:20:19 +0100

    Setup tests

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Module Docstring
+"""
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
