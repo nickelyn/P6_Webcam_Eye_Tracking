9efa2dbbf30d4432085286fcb0858dad03def354
nickely <kknicklas3689@hotmail.com>
2022-03-03 08:20:07 +0100

    Set-up files

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
+import numpy as np
+import cv2 as cv
+import glob
+
+
 """
 Main module
 """
 
 
 def main():
-    print("hello world")
-
+    print
 
 if __name__ == "__main__":
     """This is executed when run from the command line"""
+    
     main()
---
+++ tests/test_cv.py
@@ -0,0 +1,20 @@
+import random
+import unittest
+import glob
+import numpy as np
+import cv2 as cv
+
+class TestCV(unittest.TestCase):
+    
+    def setUp(self):
+        capture = cv.VideoCapture(0)
+        self.seq = capture
+
+    def test_camera(self):
+            self.assertTrue(self.seq.isOpened())
+
+
+if __name__ == "__main__":
+    unittest.main()
+    
+
