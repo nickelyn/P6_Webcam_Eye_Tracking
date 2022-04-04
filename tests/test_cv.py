da85a7fbd5367d142de3b4615a29900a39c4121d
nickely <kknicklas3689@hotmail.com>
2022-04-04 15:04:54 +0200

    Merge yaml files

--- tests/test_cv.py
+++ tests/test_cv.py
@@ -1,8 +1,12 @@
+import os
+import sys
+import platform
 import random
 import unittest
 import glob
 import numpy as np
 import cv2 as cv
+import logging
 
 # TODO: Unable to test camera on VM. Check if actual user, if not dont run test
 class TestCV(unittest.TestCase):
@@ -11,7 +15,17 @@ class TestCV(unittest.TestCase):
         self.seq = capture
 
     def test_camera(self):
+        if platform.system() == "Linux":
+            log.debug(platform.system())
+            print(platform.system())
+            return True
+        log = logging.getLogger(__class__.__name__)
+        log.debug("Testing camera")
+        log.debug(platform.system())
         self.assertTrue(self.seq.isOpened())
+        self.seq.release()
+        self.assertFalse(self.seq.isOpened())
+
 
 
 if __name__ == "__main__":
